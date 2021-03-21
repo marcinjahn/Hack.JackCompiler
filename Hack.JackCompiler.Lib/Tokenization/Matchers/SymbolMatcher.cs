c59662c28cad59247217d89612285f5c67b82f12
Marcin Jahn <marcin.jahn@pl.abb.com>
2021-03-21 10:04:46 +0100

    Fix SymbolMatcher values for special symbols

--- Hack.JackCompiler.Lib/Tokenization/Matchers/SymbolMatcher.cs
+++ Hack.JackCompiler.Lib/Tokenization/Matchers/SymbolMatcher.cs
@@ -1,6 +1,5 @@
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Web;
 using Hack.JackCompiler.Lib.JackConstants;
 
 namespace Hack.JackCompiler.Lib.Tokenization.Matchers
@@ -24,12 +23,20 @@ namespace Hack.JackCompiler.Lib.Tokenization.Matchers
         protected override MatchResult GetMatchingResult(Match match)
         {
             return MatchResult.CreateMatching(
-                new Token(
-                    match.Value == "<" || match.Value == ">"?
-                        HttpUtility.UrlEncode(match.Value) :
-                        match.Value,
-                    TokenType.Symbol),
+                new Token(GetValue(match.Value), TokenType.Symbol),
                 match.Value.Length);
+        }
+
+        private static string GetValue(string rawValue)
+        {
+            return rawValue switch
+            {
+                "<" => "&lt;",
+                ">" => "&gt;",
+                "\"" => "&quot;",
+                "&" => "&amp;",
+                _ => rawValue
+            };
         }
     }
 }
