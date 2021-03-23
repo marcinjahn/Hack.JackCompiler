687a62dbc9c21a10f0319d8c239fa317324f63f1
Marcin Jahn <marcin.jahn@pl.abb.com>
2021-03-23 19:02:49 +0100

    Remove SharpSerializer

--- Hack.JackCompiler.XmlUtilities/ElementExtensions.cs
+++ Hack.JackCompiler.XmlUtilities/ElementExtensions.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections;
-using System.IO;
 using Hack.JackCompiler.Lib.Parsing;
 using Hack.JackCompiler.Lib.Parsing.Terminal;
 using Microsoft.Extensions.Options;
-using Polenter.Serialization;
 
 namespace Hack.JackCompiler.XmlUtilities
 {
@@ -12,23 +8,6 @@ namespace Hack.JackCompiler.XmlUtilities
     {
         public static string ToXml(this IElement element)
         {
-            // var serializer = new System.Xml.Serialization.SimpleXmlSerializer(element.GetType());
-            // using var stream = new MemoryStream();
-            // serializer.Serialize(stream, element);
-            //
-            // using var reader = new StreamReader(stream);
-            // var data = reader.ReadToEnd();
-            //
-            // return data;
-
-            // using var stream = new MemoryStream();
-            // var serializer = new SharpSerializer();
-            // serializer.Serialize(element, stream);
-            // stream.Position = 0;
-            // using var reader = new StreamReader(stream);
-            // var data = reader.ReadToEnd();
-            // return data;
-
             var options = new SimpleXmlSerializerOptions<IElement>
             {
                 Indentation = 2,
